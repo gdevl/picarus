bd020bd6afa133ceb77408048258e625c863e7bc
gdevl <throwawayemailaddy@protonmail.com>
2021-02-01 16:44:04 -0500

    delete postlikes route tested and working

--- routes/api/postlikes.js
+++ routes/api/postlikes.js
@@ -1,37 +1,70 @@
-const express = require("express");
-const { asyncErrorHandler, handleValidationErrors } = require("../../utils");
+const express = require('express');
+const { asyncErrorHandler, handleValidationErrors } = require('../../utils');
 
-const { PostLike, User } = require("../../db/models");
+const { PostLike, User } = require('../../db/models');
 
 const router = express.Router();
 
 router.get(
-  "/",
-  asyncErrorHandler(async function (req, res, next) {
-    const postLikes = await PostLike.findAll();
-    res.json({ postLikes });
-  })
+    '/',
+    asyncErrorHandler(async function (req, res, next) {
+        const postLikes = await PostLike.findAll();
+        res.json({ postLikes });
+    })
 );
 
+router.get(
+    '/:id',
+    asyncErrorHandler(async (req, res, next) => {
+        const postLikeId = parseInt(req.params.id);
+        const postLike = await PostLike.findByPk(postLikeId);
+        const { id, uid, pid } = postLike;
+
+        if (!postLike) {
+            const err = new Error('No Like Found');
+            err.status = 404;
+            err.title = 'Invalid Request';
+            err.errors = ['Like does not exist'];
+            return next(err);
+        }
+
+        res.json({ postLike: { id, uid, pid } });
+    })
+);
 
 router.post(
-  "/",
-  asyncErrorHandler(async (req, res) => {
-    const { uid, pid } = req.body;
-    const postLike = await PostLike.create({ uid, pid });
+    '/',
+    asyncErrorHandler(async (req, res) => {
+        const { uid, pid } = req.body;
+        const postLike = await PostLike.create({ uid, pid });
 
-    const newPostLike = await PostLike.findByPk(postLike.id, {
-      include: {
-        model: User, attributes: ['displayName']
-      }
+        const newPostLike = await PostLike.findByPk(postLike.id, {
+            include: {
+                model: User,
+                attributes: ['displayName'],
+            },
+        });
+
+        if (newPostLike) {
+            return res.json(newPostLike);
+        }
+
+        res.json('An error occurred during like creation.');
     })
+);
 
-    if (newPostLike) {
-      return res.json(newPostLike);
-    }
+router.delete(
+    '/:id',
+    asyncErrorHandler(async (req, res) => {
+        const postLikeId = parseInt(req.params.id);
+        const postLike = await PostLike.findByPk(postLikeId);
 
-    res.json("An error occurred during like creation.");
-  })
+        if (postLike) {
+            await postLike.destroy();
+            return res.json({ deletedPostLikeId: postLikeId });
+        }
+        res.json('An error occurred during like destruction.');
+    })
 );
 
 module.exports = router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,128 +1,140 @@
-const express = require("express");
-const bcrypt = require("bcryptjs");
-const { check } = require("express-validator");
-const { asyncErrorHandler, handleValidationErrors } = require("../../utils");
-const { getUserToken, requireAuth } = require("../../auth");
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const { check } = require('express-validator');
+const { asyncErrorHandler, handleValidationErrors } = require('../../utils');
+const { getUserToken, requireAuth } = require('../../auth');
 
-const { User } = require("../../db/models");
+const { User } = require('../../db/models');
 
 const router = express.Router();
 
 const validateSignUp = [
-  check("displayName")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a display name")
-    .isLength({ max: 50 })
-    .withMessage("Display name must not be more than 50 characters long")
-    .custom((value) => {
-      return User.findOne({ where: { displayName: value } }).then((user) => {
-        if (user) {
-          return Promise.reject(
-            "The display name provided is already in use by another account"
-          );
-        }
-      });
-    }),
-  check("email")
-    .isEmail()
-    .withMessage("Please provide a valid email.")
-    .isLength({ max: 255 })
-    .withMessage("Email Address must not be more than 255 characters long")
-    .custom((value) => {
-      return User.findOne({ where: { email: value } }).then((user) => {
-        if (user) {
-          return Promise.reject(
-            "The email address provided is already in use by another account"
-          );
-        }
-      });
-    }),
-  check("password")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a password.")
-    .isLength({ min: 8 })
-    .withMessage("Password must be at least 8 characters long.")
-    .isLength({ max: 50 })
-    .withMessage("Password must not be more than 50 characters long"),
-  check("confirmPassword")
-    .isLength({ max: 50 })
-    .withMessage("Confirm Password must not be more than 50 characters long")
-    .custom((value, { req }) => {
-      if (value !== req.body.password) {
-        throw new Error("Confirm Password does not match Password");
-      }
-      return true;
-    }),
-  handleValidationErrors,
+    check('displayName')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a display name')
+        .isLength({ max: 50 })
+        .withMessage('Display name must not be more than 50 characters long')
+        .custom((value) => {
+            return User.findOne({ where: { displayName: value } }).then(
+                (user) => {
+                    if (user) {
+                        return Promise.reject(
+                            'The display name provided is already in use by another account'
+                        );
+                    }
+                }
+            );
+        }),
+    check('email')
+        .isEmail()
+        .withMessage('Please provide a valid email.')
+        .isLength({ max: 255 })
+        .withMessage('Email Address must not be more than 255 characters long')
+        .custom((value) => {
+            return User.findOne({ where: { email: value } }).then((user) => {
+                if (user) {
+                    return Promise.reject(
+                        'The email address provided is already in use by another account'
+                    );
+                }
+            });
+        }),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.')
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long.')
+        .isLength({ max: 50 })
+        .withMessage('Password must not be more than 50 characters long'),
+    check('confirmPassword')
+        .isLength({ max: 50 })
+        .withMessage(
+            'Confirm Password must not be more than 50 characters long'
+        )
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Confirm Password does not match Password');
+            }
+            return true;
+        }),
+    handleValidationErrors,
 ];
 
 router.get(
-  "/",
-  asyncErrorHandler(async function (req, res, next) {
-    const users = await User.findAll();
-    res.json({ users });
-  })
+    '/',
+    asyncErrorHandler(async function (req, res, next) {
+        const users = await User.findAll();
+        res.json({ users });
+    })
 );
 
 router.get(
-  "/:id",
-  asyncErrorHandler(async (req, res, next) => {
-    const userId = parseInt(req.params.id);
-    const user = await User.findByPk(userId);
-    const { id, displayName, email } = user;
+    '/:id',
+    asyncErrorHandler(async (req, res, next) => {
+        const userId = parseInt(req.params.id);
+        const user = await User.findByPk(userId);
+        const { id, displayName, email } = user;
 
-    if (!user) {
-      const err = new Error("No User Found");
-      err.status = 404;
-      err.title = "Invalid User";
-      err.errors = ["User does not exist"];
-      return next(err);
-    }
+        if (!user) {
+            const err = new Error('No User Found');
+            err.status = 404;
+            err.title = 'Invalid User';
+            err.errors = ['User does not exist'];
+            return next(err);
+        }
 
-    res.json({ user: { id, displayName, email } });
-  })
+        res.json({ user: { id, displayName, email } });
+    })
 );
 
 router.post(
-  "/",
-  validateSignUp,
-  asyncErrorHandler(async (req, res) => {
-    const { displayName, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({ displayName, email, hashedPassword });
+    '/',
+    validateSignUp,
+    asyncErrorHandler(async (req, res) => {
+        const { displayName, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ displayName, email, hashedPassword });
 
-    const token = getUserToken(user);
-    res.cookie("token", token);
-    res.status(201).json({
-      user: { id: user.id, displayName: user.displayName, email: user.email },
-      token,
-    });
-  })
+        const token = getUserToken(user);
+        res.cookie('token', token);
+        res.status(201).json({
+            user: {
+                id: user.id,
+                displayName: user.displayName,
+                email: user.email,
+            },
+            token,
+        });
+    })
 );
 
 router.post(
-  "/signin",
-  asyncErrorHandler(async function (req, res, next) {
-    const { email, password } = req.body;
-    const user = await User.findOne({
-      where: {
-        email,
-      },
-    });
+    '/signin',
+    asyncErrorHandler(async function (req, res, next) {
+        const { email, password } = req.body;
+        const user = await User.findOne({
+            where: {
+                email,
+            },
+        });
 
-    if (!user || !user.validatePassword(password)) {
-      const err = new Error("Login failed");
-      err.status = 401;
-      err.title = "Login failed";
-      err.errors = ["The provided credentials were invalid."];
-      return next(err);
-    }
-    const token = getUserToken(user);
-    res.json({
-      token,
-      user: { id: user.id, displayName: user.displayName, email: user.email },
-    });
-  })
+        if (!user || !user.validatePassword(password)) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+        const token = getUserToken(user);
+        res.json({
+            token,
+            user: {
+                id: user.id,
+                displayName: user.displayName,
+                email: user.email,
+            },
+        });
+    })
 );
 
 module.exports = router;
