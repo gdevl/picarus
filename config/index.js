63e411a5ee5ebf5dc33d8112208351d54f3593b5
gdevl <throwawayemailaddy@protonmail.com>
2021-02-08 17:38:00 -0500

    ssl tinkering

--- config/index.js
+++ config/index.js
@@ -1,19 +1,22 @@
 module.exports = {
-  environment: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 8080,
-  db: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-  },
-  jwtConfig: {
-    secret: process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  },
-  awsKeys: {
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY,
-    region: process.env.REGION,
-  },
+    environment: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 8080,
+    db: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        host: process.env.DB_HOST,
+    },
+    jwtConfig: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    },
+    awsKeys: {
+        accessKeyId: process.env.ACCESS_KEY_ID,
+        secretAccessKey: process.env.SECRET_ACCESS_KEY,
+        region: process.env.REGION,
+    },
+    ssl: {
+        rejectUnauthorized: false,
+    },
 };
